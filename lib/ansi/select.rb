fbd1a35f31d43df74273d837c8e68a3c02a24ac2
Volodymyr Shatsky <shockone89@gmail.com>
2015-09-28 10:43:34 +0300

    Use polymorphism.

--- lib/ansi/select.rb
+++ lib/ansi/select.rb
@@ -4,6 +4,25 @@ require "io/console"
 
 module Ansi
   class Select
+
+
+    # @param [Array<#to_s>] options
+    #
+    # @return [#to_s] option
+    def self.select(options)
+      SingleSelectImpl.new(options).select
+    end
+
+    # @param [Array<#to_s>] options
+    #
+    # @return [Array<#to_s>] option
+    def self.multi_select(options)
+      MultiSelectImpl.new(options).select
+    end
+  end
+
+
+  class SelectImpl
     CODES = {
       standout_mode: `tput rev`,
       exit_standout_mode: `tput rmso`,
@@ -12,7 +31,6 @@ module Ansi
       carriage_return_key: `tput cr`
     }
 
-    # @param [Array<#to_s>] options
     def initialize(options)
       @options = options
 
@@ -20,7 +38,6 @@ module Ansi
       @cursor_line_index = 0
     end
 
-    # @return [#to_s] option
     def select
       print_options
       answer = ask_to_choose
@@ -29,12 +46,6 @@ module Ansi
       answer
     ensure
       tty.close
-    end
-
-    # @return [Array<#to_s>] option
-    def multi_select
-      @chosen = []
-      select
     end
 
     private
@@ -74,6 +85,51 @@ module Ansi
 
     # @return [#to_s]
     def ask_to_choose
+      raise NotImplementedError
+    end
+
+    # @param [Fixnum] index
+    # @param [Boolean] highlight
+    def print_line(index, highlight)
+      go_to_line(index)
+
+      if highlight
+        tty.print(CODES[:standout_mode] + prefix(index) + @options[index] + CODES[:exit_standout_mode])
+      else
+        tty.print(prefix(index) + @options[index])
+      end
+    end
+
+    # @param [Fixnum] index
+    def highlight_line(index)
+      print_line(@highlighted_line_index, false)
+      print_line(index, true)
+
+      @highlighted_line_index = index
+    end
+
+    # @param [Fixnum] index
+    def go_to_line(index)
+      if index == @cursor_line_index
+        # do nothing
+      elsif index > @cursor_line_index
+        (index - @cursor_line_index).times { tty.print CODES[:cursor_down] }
+      else
+        (@cursor_line_index - index).times { tty.print CODES[:cursor_up] }
+      end
+
+      @cursor_line_index = index
+      tty.print CODES[:carriage_return_key]
+    end
+
+    def prefix(index)
+      raise NotImplementedError
+    end
+  end
+
+  class SingleSelectImpl < SelectImpl
+    # @return [#to_s]
+    def ask_to_choose
       loop do
         input = listen_carefully_to_keyboard
 
@@ -99,40 +155,50 @@ module Ansi
       end
     end
 
-    # @param [Fixnum] index
-    # @param [Boolean] highlight
-    def print_line(index, highlight)
-      go_to_line(index)
+    private
 
-      prefix = @chosen ? (@chosen[index] ? '[x] ' : '[ ] ') : ''
+    def prefix(index)
+      ''
+    end
+  end
 
-      if highlight
-        tty.print(CODES[:standout_mode] + prefix + @options[index] + CODES[:exit_standout_mode])
-      else
-        tty.print(prefix + @options[index])
+  class MultiSelectImpl < SelectImpl
+    def initialize(options)
+      super
+      @chosen = []
+    end
+
+    private
+
+    # @return [#to_s]
+    def ask_to_choose
+      loop do
+        input = listen_carefully_to_keyboard
+
+        case input
+        when "\u0003", "q"
+          exit(0)
+        when " "
+          if @chosen
+            @chosen[@cursor_line_index] = !@chosen[@cursor_line_index]
+            print_line(@cursor_line_index, true)
+          end
+        when CODES[:carriage_return_key]
+          if @chosen
+            break @chosen.map.with_index { |value, index| @options[index] if value }.compact
+          else
+            break @options[@highlighted_line_index]
+          end
+        when "\e[A", "k", CODES[:cursor_up]
+          highlight_line(@highlighted_line_index - 1) unless @highlighted_line_index == 0
+        when "\e[B", "j", CODES[:cursor_down]
+          highlight_line(@highlighted_line_index + 1) unless @highlighted_line_index == @options.size - 1
+        end
       end
     end
 
-    # @param [Fixnum] index
-    def highlight_line(index)
-      print_line(@highlighted_line_index, false)
-      print_line(index, true)
-
-      @highlighted_line_index = index
-    end
-
-    # @param [Fixnum] index
-    def go_to_line(index)
-      if index == @cursor_line_index
-        # do nothing
-      elsif index > @cursor_line_index
-        (index - @cursor_line_index).times { tty.print CODES[:cursor_down] }
-      else
-        (@cursor_line_index - index).times { tty.print CODES[:cursor_up] }
-      end
-
-      @cursor_line_index = index
-      tty.print CODES[:carriage_return_key]
+    def prefix(index)
+      @chosen[index] ? '[x] ' : '[ ] '
     end
   end
 end
